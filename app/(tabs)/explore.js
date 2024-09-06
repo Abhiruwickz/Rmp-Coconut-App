d261bcf41ceab003471ada94fc32672d1a401e93
Abhiru Wickramaarachchi <136422566+Abhiruwickz@users.noreply.github.com>
2024-09-06 13:08:59 -0500

    Created project with necessary dependancies

---
+++ app/(tabs)/explore.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const explore = () => {
+  return (
+    <View>
+      <Text className="font-bold text-center text-slate-900">explore</Text>
+    </View>
+  )
+}
+
+export default explore
