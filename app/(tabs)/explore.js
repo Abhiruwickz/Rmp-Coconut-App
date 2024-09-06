26e23b6ca015c6ab2939c1d5bc093447bc61f454
Abhiru Wickramaarachchi <136422566+Abhiruwickz@users.noreply.github.com>
2024-09-06 16:04:35 -0500

    Add firebase config to the app

--- app/(tabs)/explore.js
+++ app/(tabs)/explore.js
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text,Image,TouchableOpacity } from 'react-native'
 import React from 'react'
+
 
 const explore = () => {
   return (
-    <View>
-      <Text className="font-bold text-center text-slate-900">explore</Text>
+    <View className="justify-center items-center bg-white flex-1">
+      <View className="-mt-16">
+      <Image
+      source={require("../../assets/images/Rmplogo.png")}
+      className="rounded-lg w-[170px] h-[170px] justify-center"
+      >
+      </Image>
+      <Text className="font-bold text-center text-slate-900 items-center mt-6">Welcome to the Rmp Coconuts!</Text>
+    </View>
+    <View className="mt-10">
+      <TouchableOpacity className="bg-orange-600 rounded-lg w-[200px] h-[40px] mb-8 ">
+        <Text className="text-center text-white font-semibold text-lg mt-1">Log In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity className="bg-orange-600 rounded-lg w-[200px] h-[40px] ">
+        <Text className="text-center text-white font-semibold text-lg mt-1">Sign Up</Text>
+      </TouchableOpacity>
+    </View>
     </View>
   )
 }
---
+++ firebaseConfig.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+import { initializeAuth,getReactNativePersistence } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAGLa2lnkLHEDh9fs7OuxVmJsVQY0a9GPI",
+    authDomain: "rmp-coconuts.firebaseapp.com",
+    projectId: "rmp-coconuts",
+    storageBucket: "rmp-coconuts.appspot.com",
+    messagingSenderId: "743970093389",
+    appId: "1:743970093389:web:a42b5668b0713fd52a1f04"
+  };
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const firebase_auth = initializeAuth(app);
+
+export const analytics = getAnalytics(app);
+export const storage = getStorage(app);
+export const firestore = getFirestore(app);
+export const Real_time_database = getDatabase(app);
